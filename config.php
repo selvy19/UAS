a20592ac82f854c1ccfdfd1646808b49a7ec01ba
selvy19 <78544505+selvy19@users.noreply.github.com>
2021-04-10 20:03:33 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$con = mysqli_connect("localhost","id16570181_users","Us3rs@2020.12345","id16570181_uas_web");
+// Check connection
+if (mysqli_connect_error())
+{
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+?>
